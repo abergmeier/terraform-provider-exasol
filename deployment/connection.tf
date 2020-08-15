d05794e708d24787e7b18ff69eb7f1bd18841d31
Bergmeier, Andreas <Andreas.Bergmeier@otto.de>
2020-08-15 22:58:49 +0200

    Add Connection examples from Exasol to Terraform.

--- deployment/connection.tf
+++ deployment/connection.tf
@@ -1,6 +1,32 @@
-resource "exasol_connection" "dummy" {
-  name     = "test"
-  to       = "192.168.1.1"
-  username = "foo"
-  password = "bar"
+// See https://docs.exasol.com/sql/create_connection.htm
+
+resource "exasol_connection" "ftp_connection" {
+  name     = "ftp_connection"
+  to       = "ftp://192.168.1.1/"
+  username = "agent_007"
+  password = "secret"
+}
+
+resource "exasol_connection" "exa_connection" {
+  name     = "exa_connection"
+  to       = "192.168.6.11..14:8563"
+}
+
+resource "exasol_connection" "ora_connection" {
+  name = "ora_connection"
+  to   = <<EOF
+    (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.6.54)(PORT = 1521))
+    (CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = orcl)))
+EOF
+}
+
+resource "exasol_connection" "jdbc_connection_1" {
+  name = "jdbc_connection_1"
+  to   = "jdbc:mysql://192.168.6.1/my_db"
+}
+
+resource "exasol_connection" "jdbc_connection_2" {
+  name = "jdbc_connection_2"
+  to   = "jdbc:postgresql://192.168.6.2:5432/my_db?stringtype=unspecified"
 }
