27f7cc9002768c4926cb469a09d9f327658efa13
Andreas Bergmeier <abergmeier@gmx.net>
2020-09-08 21:16:02 +0200

    Introduce ErrorEmptyName
    Use to indicate when a name is empty.

--- pkg/argument/argument.go
+++ pkg/argument/argument.go
@@ -1,14 +1,21 @@
 package argument
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/abergmeier/terraform-exasol/internal"
 )
 
+var (
+	// ErrorEmptyName is an error because name has to have a meaningful value
+	ErrorEmptyName = errors.New("Empty name not allowed")
+)
+
 // Name extracts name of Data
 func Name(d internal.Data) (string, error) {
 	name := d.Get("name").(string)
+
 	if name == "" {
 		return "", fmt.Errorf("Empty name for %s", d)
 	}
